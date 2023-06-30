9ef6124d32592bae18b72a48976033a7111a6166
Danica Jefferies <danjefferies2001@gmail.com>
2023-06-29 20:54:09 -0400

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/usrtk-seed-oils-project/',
+  base: '/seed-oils-health-risks/',
   plugins: [svelte()]
 })
